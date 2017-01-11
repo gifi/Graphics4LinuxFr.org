5d35c0bac1889afb508f6cce27c0c1a57411b65c
gifi <nieric@outlook.com>
2017-01-11 11:13:24 +0000

    License blooper

--- statistics_linuxfr.R
+++ statistics_linuxfr.R
@@ -1,4 +1,4 @@
-# Copyright 2016, 1gfypobb6sznxm9@jetable.org
+# Copyright 2017, 1gfypobb6sznxm9@jetable.org
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
